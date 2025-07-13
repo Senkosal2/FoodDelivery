8a6c7017cb0035b4ec4f22b1802c794e55dbc855
Senkosal <senkosalker2@gmail.com>
2025-07-14 05:43:42 +0700

    updates

--- main/src/main/java/group/fooddelivery/main/utils/Global.java
+++ main/src/main/java/group/fooddelivery/main/utils/Global.java
@@ -1,10 +1,45 @@
 package group.fooddelivery.main.utils;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Global {
-    public static enum orderStatus{
+
+    public static enum UserType {
+        ADMIN,
+        USER,
+        DELIVERY,
+        MERCHANT
+    }
+
+    public static enum OrderStatus{
         CANCELED,
         PENDING,
         SUCCESS, 
         FAILED
     }
+
+    public static String encrptPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
+    }
+
+    public static boolean verifyPassword(String password, String hashedPassword) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.matches(password, hashedPassword);
+    } 
+
+    // generic class for converting List<DTO> to List<Entity> or List<Entity> to List<DTO>
+    // if ToConvert is DTO then ConvertedTo is Entity
+    // if ToConvert is Entity then ConvertedTo is DTO 
+    // public static <ToConvert, ConvertedTo> List<ConvertedTo> toList(
+    //         List<ToConvert> sources,
+    //         BiFunction<Object, ToConvert, ConvertedTo> converter,
+    //         Object instance) {
+
+    //     List<ConvertedTo> results = new ArrayList<>();
+    //     for (ToConvert source : sources) {
+    //         results.add(converter.apply(instance, source));  // Apply method on instance
+    //     }
+    //     return results;
+    // }
 }
