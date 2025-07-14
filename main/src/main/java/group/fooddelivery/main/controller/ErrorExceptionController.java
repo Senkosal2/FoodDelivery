1a70949df19728939b22f67e5673b6bae6c7d68c
Senkosal <senkosalker2@gmail.com>
2025-07-14 19:40:50 +0700

    updated

--- main/src/main/java/group/fooddelivery/main/controller/ErrorExceptionController.java
+++ main/src/main/java/group/fooddelivery/main/controller/ErrorExceptionController.java
@@ -1,12 +1,19 @@
 package group.fooddelivery.main.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import group.fooddelivery.main.dto.response.InvalidResponse;
 import jakarta.servlet.RequestDispatcher;
@@ -14,7 +21,10 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class ErrorExceptionController implements ErrorController {
-    
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @RequestMapping("/error")
     public ResponseEntity<InvalidResponse<List<String>>> routeNotFound(HttpServletRequest request) {
         
@@ -32,7 +42,15 @@ public class ErrorExceptionController implements ErrorController {
             // invalidResponse.setMessage("Current uri does not exist: " + request.getRequestURL());
             invalidResponse.setUri(List.of(
                 "/api/users",
-                "/api/addresses"
+                "/api/addresses",
+                "/api/foods",
+                "/api/categories",
+                "/api/orders",
+                "/api/api/order-details",
+                "/api/reviews",
+                "/api/toppings",
+                "/api/deliveries",
+                "/api/delivery-details"
             ));
         } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
             invalidResponse = new InvalidResponse<>();
